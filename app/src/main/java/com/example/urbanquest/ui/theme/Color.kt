92acc4d2d62de1bd31ab0ae1fe16eaaa8dab6de2
anton <anton.savitskiy.02@mail.ru>
2024-04-15 03:28:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/urbanquest/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.urbanquest.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GreenUQ = Color(0xFF65D067)
+val BlackGreen = Color (0xFF002103)
+val ButtonColor = Color(0xFFC9EA63)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Grey = Color(0xFF6B6B6B)
+val BlackGrey = Color(0xFF484848)
+val Red = Color(0xFFFF2828)
+
+val Pink80 = Color(0xFFEFB8C8)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
